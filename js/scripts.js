48835b223bd1b084d2431098b74341e2c288a544
DavidNganga <ngashiedavid@gmail.com>
2018-02-23 14:35:43 +0300

    comments

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var nuMber = function(person1) {
+	if (person1 % 3) {
+		return ping;
+	}
+}
+
+
+
+
+$(document).ready(function() {
+			$("form#pingypong").submit(function(event) {
+						event.preventDefault();
+						var year = parseInt($("input#pingpongy").val());
+						var result = (person1);

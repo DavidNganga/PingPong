af5a165de5928a42da0099c0f7833bfa29e5373c
DavidNganga <ngashiedavid@gmail.com>
2018-02-24 11:39:17 +0300

    comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,28 @@
-var nuMber = function(person1) {
-	if (person1 % 3) {
-		return ping;
-	}
-}
+var pingpong = function(number) {
+	for (var i = 0; i <= number; i++) {
+		if (number % 15 === 0) {
 
+			console.log("pingpong");
+		} else if (number % 5 === 0) {
 
+			console.log("pong");
+		} else if (number % 3 === 0) {
 
+			console.log("ping");
+		} else {
+			console.log(number);
+		}
+	};
+};
 
 $(document).ready(function() {
-			$("form#pingypong").submit(function(event) {
-						event.preventDefault();
-						var year = parseInt($("input#pingpongy").val());
-						var result = (person1);
+	$("form#pingypong").click(function(event) {
+		event.preventDefault();
+
+		var i = parseInt($("input#pingpongy").val());
+		var result = pingpong(i);
+		$("#result").text(result);
+
+		$(".result").show();
+	});
+});

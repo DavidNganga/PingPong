bdfc982a4ad8b15744ca0438e91c56708b2f88a6
DavidNganga <ngashiedavid@gmail.com>
2018-02-25 18:06:51 +0300

    comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,32 @@
 var pingpong = function(number) {
 	for (var i = 0; i <= number; i++) {
 		if (number % 15 === 0) {
-
-			console.log("pingpong");
-		} else if (number % 5 === 0) {
-
-			console.log("pong");
-		} else if (number % 3 === 0) {
-
-			console.log("ping");
-		} else {
-			console.log(number);
+			$("div").html("<span class='.result'>pingpong</span>");
+			return "pingpong"
+			//console.log("pingpong");
 		}
-	};
+
+
+		//console.log("pingpong");
+	} else if (number % 5 === 0) {
+		$("div").html("<span class='.result'>pong</span>");
+		console.log("pong");
+	} else if (number % 3 === 0) {
+		$("div").html("<span class='.result'>ping</span>");
+		console.log("ping");
+	} else {
+		console.log(number);
+	}
+};
 };
 
 $(document).ready(function() {
-	$("form#pingypong").click(function(event) {
+	$("form#pingypong").submit(function(event) {
 		event.preventDefault();
 
 		var i = parseInt($("input#pingpongy").val());
 		var result = pingpong(i);
-		$("#result").text(result);
+		$(".result").text(result);
 
 		$(".result").show();
 	});
